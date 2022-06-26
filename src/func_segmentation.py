1b1aac552c009fa92631e41accd68b60dd766bd8
ahesmaeili <80411057+AHEsmaeili@users.noreply.github.com>
2022-06-26 12:02:41 +0200

    fixed syntax in Otsu

--- src/func_segmentation.py
+++ src/func_segmentation.py
@@ -146,7 +146,7 @@ def segmentation_otsu(im:ArrayLike,
         mask[frame,...] = morphology.remove_small_holes(
             morphology.remove_small_objects(
                 fill, min_size=minRemoveSize,
-            removeHoleSize)
+            removeHoleSize))
 
     return mask
 
